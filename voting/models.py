3223b736874e25ccda5e33da05cfbdd9fba380f7
Aparna C <aparnaammuzz96@gmail.com>
2024-11-08 12:31:23 +0530

    canditate model created

--- voting/models.py
+++ voting/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Candidate(models.Model):
+    candidate_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    manifesto = models.TextField()
+    department = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
+    photo = models.ImageField(upload_to='candidate_photos/', blank=True, null=True)
+    vote_count = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return f"{self.name} - {self.position}"
+
