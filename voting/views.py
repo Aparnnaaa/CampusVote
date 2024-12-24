8fa6c469b3237643f4ebb87163eba5a228ec67a1
Aparna C <aparnaammuzz96@gmail.com>
2024-12-24 22:46:39 +0530

    created voter frm

--- voting/views.py
+++ voting/views.py
@@ -2,7 +2,9 @@ from .utils import voter_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.hashers import check_password
-from .models import Voter, Election, Candidate
+from .models import Voter, Election, Candidate, Vote
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def voter_login(request):
@@ -51,3 +53,26 @@ def election_details(request, election_id):
     election = get_object_or_404(Election, pk=election_id)
     candidates = election.candidates.all()
     return render(request, 'election_details.html', {'election': election, 'candidates': candidates})
+
+
+@login_required
+def cast_vote(request, election_id):
+    voter = request.user
+    election = get_object_or_404(Election, pk=election_id)
+
+    if request.method == 'POST':
+        candidate_id = request.POST.get('candidate_id')
+        if voter.has_voted:
+            return HttpResponse("You have already voted n this election.")
+
+        candidate = get_object_or_404(Candidate, pk=candidate_id)
+        if candidate.election.id != election.id:
+            return HttpResponse("Invalid candidate for this election.")
+
+        Vote.objects.create(
+            voter=voter, candidate=candidate, election=election)
+        voter.has_voted = True
+        voter.save()
+        return HttpResponse("Thank you for voting!")
+
+    return render(request, 'cast_vote.html', {'election': election})
